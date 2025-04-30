ea0fb40c0e43ab20c88a77a2f8aadbbb0265e273
Leo Santander <santandernycz.ls@gmail.com>
2025-04-30 21:01:11 +0100

    atualizacao GNL

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsantand <lsantand@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 23:13:56 by lsantand          #+#    #+#             */
+/*   Updated: 2025/04/29 18:35:23 by lsantand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <unistd.h>
+
+// join e free
+static char		*h_free(char *s, char *buf)
+{
+	char	*temp;
+
+	temp = ft_strjoin(s, buf);
+	free(s);
+	return (temp);
+}
+
+// deleta a linha encontrada
+static char		*h_find(char *s)
+{
+	int		i;
+	int		j;
+	char	*line;
+
+	i = 0,
+	// len da primeira linha
+	while (s[i] && s[i] != '\n')
+		i++;
+	// se terminar com \0, entao retorna Null
+	if (!s[i])
+	{
+		free(s);
+		return (NULL);
+	}
+	//(len do arquivo) - (len da primeira linha + 1)
+	line = ft_calloc((ft_strlen(s) - i + 1), sizeof(char));
+	i++;
+	j = 0;
+	while (s[i])
+		line[j++] = s[i++];
+	free(s);
+	return (line);
+}
+
+// retorna a linha
+static char		*h_line(char *s)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	// se não tiver linha, retorna Null
+	if (!s[i])
+		return (NULL);
+	// vai até o '\0'
+	while (s[i] && s[i] != '\n')
+	{
+		line[i] = s[i];
+		i++;
+	}
+	// se S terminar em '\0' ou '\n', troca o fim da linha por '\n'
+	if (s[i] && s[i] == '\n')
+		line[i++] = '\n';
+	return (line);
+}
+
+static char		*h_read_file(int fd, char *res)
+{
+	char 	*s;
+	int		byte_read;
+
+	//malloc se o res não existir
+	if (!res)
+		res = ft_calloc(1, 1);
+	//malloc para S
+	s = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	byte_read = 1;
+	while (byte_read > 0)
+	{
+		//enquanto o fim da linha nao for lido
+		byte_read = read(fd, s, BUFFER_SIZE);
+		if (byte_read == -1)
+		{
+			free(s);
+			return (NULL);
+		}
+		// 0 para o fim, para evitar mem leak
+		s[byte_read] = 0;
+		// join e free
+		res = h_free(res, s);
+		// para se encontrar a '\n'
+		if (ft_strchr(s, '\n'))
+			break;
+	}
+	free(s);
+	return (res);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*s;
+	char 		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	s = h_read_file(fd, s);
+	if (!s)
+		return (NULL);
+	line = h_line(s);
+	s = h_find(s);
+	return (line);
+}
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsantand <lsantand@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 23:14:11 by lsantand          #+#    #+#             */
+/*   Updated: 2025/04/29 18:42:25 by lsantand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		sizetotal;
+	char	*res;
+	int		i;
+	int		j;
+
+	i = 0;
+	sizetotal = ft_strlen(s1) + ft_strlen(s2);
+	res = malloc(sizeof(char) * (sizetotal + 1));
+	if (!res || !s1 || !s2)
+		return (NULL);
+	while (s1[i] != 0)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != 0)
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[sizetotal] = 0;
+	return (res);
+}
+
+char	*ft_strchr(const char *s, int n )
+{
+	char	*str;
+
+	str = (char *)s;
+	while (*str != n && *str != 0)
+		str++;
+	if (*str == n)
+		return (str);
+	else
+		return (NULL);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*res;
+
+	res = malloc(size * count);
+	if (!res)
+		return (NULL);
+	ft_bzero(res, size * count);
+	return (res);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
