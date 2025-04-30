f9302af1290858ea754d2bef251ae7d3412f7e53
Leo Santander <santandernycz.ls@gmail.com>
2025-04-30 21:08:20 +0100

    Atualizacao GNL

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-<<<<<<< HEAD
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -83,106 +82,8 @@ size_t	ft_strlen(const char *s)
 {
 	int	i;
 
-=======
-#include <unistd.h>
-
-size_t	ft_strlen(const char	*s)
-{
-	size_t	i;
-
->>>>>>> 8a671c5251df3b4f96d90edd7a1aac1591c7159d
 	i = 0;
 	while (s[i])
 		i++;
 	return (i);
-<<<<<<< HEAD
 }
-=======
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	int	i;
-
-	i = 0;
-	if (!s)
-		return (NULL);
-	while (s[i])
-	{
-		if (s[i] == (char) c)
-			return ((char *)&s[i]);
-		i++;
-	}
-	return (NULL);
-}
-
-char	*ft_strdup(const char *s)
-{
-	size_t		size;
-	size_t		i;
-	char		*cp_str;
-
-	i = 0;
-	while (s[i])
-		i++;
-	size = (i + 1);
-	cp_str = (char *)malloc(size * sizeof(char));
-	if (!cp_str)
-		return (NULL);
-	i = 0;
-	while (i < size)
-	{
-		cp_str[i] = s[i];
-		i++;
-	}
-	return (cp_str);
-}
-
-char	*ft_strjoin(char	*s1, char const	*s2)
-{
-	size_t	i;
-	size_t	j;
-	char	*str;
-
-	if (!s1)
-		s1 = ft_strdup("");
-	if (!s1 || !s2)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-		i++;
-	j = 0;
-	while (s2[j])
-		j++;
-	str = malloc(sizeof(char) * (i + j + 1));
-	if (!str)
-		return (NULL);
-	j = 0;
-	ft_strlcpy(str, s1, i + 1);
-	while (s2[j])
-		str[i++] = s2[j++];
-	str[i] = '\0';
-	free(s1);
-	return (str);
-}
-
-size_t	ft_strlcpy(char	*dest, const char	*src, size_t	size)
-{
-	size_t	i;
-	size_t	len;
-
-	i = 0;
-	len = 0;
-	while (src[len])
-		len++;
-	if (!size)
-		return (len);
-	while (i < (size - 1) && i < len && src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (len);
-}
->>>>>>> 8a671c5251df3b4f96d90edd7a1aac1591c7159d
